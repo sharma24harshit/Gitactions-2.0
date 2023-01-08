837c19dc695d971073c7ac5d2ed2ef6e36428d3b
unknown <sharma78655533@gmail.com>
2023-01-09 01:11:09 +0530

    ga2

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,11 +1,12 @@
+import { useState } from "react"
 
 
 const counter = () => {
-  
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>counter: 0</h1>
-      
+      <h1>counter: {count}</h1>
+      <button onClick={()=>setCount(count+1)} >Inc</button>
     </div>
   )
 }
